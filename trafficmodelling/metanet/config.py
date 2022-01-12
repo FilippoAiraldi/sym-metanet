bd790ede82ffc741a16b3a0c6df74b2c14693395
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-01-12 20:06:25 +0100

    implemented config checks

--- trafficmodelling/metanet/config.py
+++ trafficmodelling/metanet/config.py
@@ -1,5 +1,5 @@
 import numpy as np
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 
 from .. import util as tm_util
 
@@ -25,11 +25,38 @@ class Config:
 
     def __post_init__(self):
         # check positivity
-        # TODO: check everythin for positivity
-        if self.T < 0:
-            raise ValueError('sampling time T cannot be negative.')
+        for key, val in asdict(self).items():
+            if not ((val >= 0).all()
+                    if isinstance(val, np.ndarray) else
+                    val >= 0):
+                raise ValueError(f'parameter \'{key}\' cannot be negative.')
 
-        # TODO: check I and O sizes
+        # check array-like parameters
+        for name, n, types in [('lanes', 'I', (int,)),
+                               ('L', 'I', (float, int)),
+                               ('C0', 'O', (float, int))]:
+            arr = getattr(self, name)
+            is_scalar = isinstance(arr, types)
+            is_array = isinstance(arr, np.ndarray)
+            if not is_scalar and not is_array or (
+                    is_array and self.lanes.dtype not in types):
+                wrong_type = arr.dtype if is_array else type(arr)
+                raise ValueError(f'parameter \'{name}\' must be a scalar '
+                                 f'or an array of types \'{types}\'; '
+                                 f'got {wrong_type} instead.')
+            sz = getattr(self, n)
+            if is_scalar:
+                arr = np.full((sz, 1), arr)
+            else:
+                if arr.ndim > 2 or (arr.ndim == 2 and arr.shape[1] != 1):
+                    raise ValueError(
+                        f'parameter \'{name}\' must be a 2D '
+                        f'column vector; got {arr.shape} instead.')
+                if arr.size != sz:
+                    raise ValueError(
+                        f'parameter \'{name}\' must have {sz} '
+                        f'elements (like \'{n}\'); got {arr.size} instead.')
+            setattr(self, name, arr)
 
         # make 1D arrays 2D
         for attr in ('lanes', 'L', 'C0'):
