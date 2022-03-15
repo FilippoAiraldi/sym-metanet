dcf2e1194dadcafbaf413cec9eefd987da044e3c
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-03-15 15:10:58 +0100

    now can import also Origin class

--- metanet/__init__.py
+++ metanet/__init__.py
@@ -4,18 +4,18 @@ __version__ = '1.0.2'
 try:
     import casadi
 except Exception as ex:
-    raise ImportError('error while importing casadi: ' + str(ex)) from ex
+    raise ImportError(f'error while importing casadi: {str(ex)}') from ex
 
 try:
     import numpy
 except Exception as ex:
-    raise ImportError('error while importing numpy: ' + str(ex)) from ex
+    raise ImportError(f'error while importing numpy: {str(ex)}') from ex
 
 
 from . import util
 from .blocks.nodes import Node
 from .blocks.links import Link, LinkWithVms
-from .blocks.origins import OnRamp, MainstreamOrigin
+from .blocks.origins import Origin, OnRamp, MainstreamOrigin
 from .blocks.destinations import Destination
 from .blocks.networks import Network
 from .sim import functional
