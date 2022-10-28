327ff50bfa0d851dd4ed3ae5a8aeb4dc207be658
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-10-28 19:06:35 +0200

    removed unused numpy util

--- src/pymetanet/util/__init__.py
+++ src/pymetanet/util/__init__.py
@@ -1,3 +1,2 @@
 from pymetanet.util.datastructures import NamedObject
-from pymetanet.util.arrays import as1darray
 from pymetanet.util.funcs import cached_property_clearer
--- tests/test_util.py
+++ tests/test_util.py
@@ -3,7 +3,6 @@ import numpy as np
 from functools import cached_property
 from pymetanet.util import (
     NamedObject,
-    as1darray,
     cached_property_clearer
 )
 
@@ -27,22 +26,6 @@ class TestUtil(unittest.TestCase):
         name = 'This is a random name'
         obj = NamedObject(name=name)
         self.assertEqual(name, obj.name)
-
-    def test_as1darray(self):
-        inputs = [
-            (1, 42),
-            (3, 42),
-            (3, [42, 42, 42]),
-            (3, (42, 42, 42)),
-            (3, (42.0, 42.0, 42.0)),
-            (3, np.array((42, 42, 42))),
-        ]
-        for input in inputs:
-            n, x = input
-            y = as1darray(x, n)
-            np.testing.assert_equal(x, y)
-            self.assertEqual(y.ndim, 1)
-            self.assertEqual(y.size, n)
 
     def test_cached_property_clearer(self):
         dummy = DummyWithCachedProperty()
