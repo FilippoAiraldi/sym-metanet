38aaa2c5bc21d7a3cf199ee118f3ec8482246a94
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-10-29 23:46:33 +0200

    implementing casadi engine

--- src/sym_metanet/engines/casadi.py
+++ src/sym_metanet/engines/casadi.py
@@ -1,6 +1,68 @@
-from typing import Dict, Literal, Type, Tuple, Union
+from typing import Dict, Literal, Type, Tuple
 import casadi as cs
-from sym_metanet.engines.base import EngineBase
+from sym_metanet.engines.core import \
+    NodesEngineBase, LinksEngineBase, OriginsEngineBase, EngineBase
+
+
+class NodesEngine(NodesEngineBase):
+    '''CasADi implementation of `sym_metanet.engines.core.NodesEngineBase`.'''
+
+
+class LinksEngine(LinksEngineBase):
+    '''CasADi implementation of `sym_metanet.engines.core.LinksEngineBase`.'''
+
+    @staticmethod
+    def get_flow(rho: cs.SX, v: cs.SX, lanes: cs.SX) -> cs.SX:
+        return rho * v * lanes
+
+    @staticmethod
+    def step_density(
+            rho: cs.SX, q: cs.SX, q_up: cs.SX,
+            lanes: cs.SX, L: cs.SX, T: cs.SX) -> cs.SX:
+        return rho + (T / lanes / L) * (q_up - q)
+
+    @staticmethod
+    def step_speed(v: cs.SX, v_up: cs.SX, rho: cs.SX, rho_down: cs.SX,
+                   Veq: cs.SX, lanes: cs.SX, L: cs.SX, tau: cs.SX, eta: cs.SX,
+                   kappa: cs.SX, T: cs.SX,
+                   q_ramp: cs.SX = None, delta: cs.SX = None,
+                   lanes_drop: cs.SX = None, phi: cs.SX = None,
+                   rho_crit: cs.SX = None):
+        relaxation = (T / tau) * (Veq - v)
+        convection = T * v / L * (v_up - v)
+        anticipation = (eta * T / tau) * (rho_down - rho) / (L * (rho + kappa))
+        v_next = v + relaxation + convection - anticipation
+        if q_ramp is not None and delta is not None:
+            v_next[0] -= \
+                (delta * T * q_ramp * v[0]) / (L * lanes * (rho[0] + kappa))
+        if lanes_drop is not None and phi is not None and rho_crit is not None:
+            v_next[-1] -= (phi * T * lanes_drop * rho[-1] * v[-1]**2) / \
+                (L * lanes * rho_crit)
+        return v_next
+
+    @staticmethod
+    def Veq(rho: cs.SX, v_free: cs.SX, rho_crit: cs.SX, a: cs.SX) -> cs.SX:
+        return v_free * cs.exp((-1 / a) * cs.power(rho / rho_crit, a))
+
+
+class OriginsEngine(OriginsEngineBase):
+    '''
+    CasADi implementation of `sym_metanet.engines.core.OriginsEngineBase`.
+    '''
+
+    @staticmethod
+    def step_queue(w: cs.SX, d: cs.SX, q: cs.SX, T: cs.SX) -> cs.SX:
+        return w + T * (d - q)
+
+    @staticmethod
+    def get_flow(d: cs.SX, w: cs.SX, C: cs.SX, r: cs.SX, rho_max: cs.SX,
+                 rho_first: cs.SX, rho_crit: cs.SX, T: cs.SX,
+                 type: Literal['in', 'out'] = 'out') -> cs.SX:
+        term1 = d + w / T
+        term3 = (rho_max - rho_first) / (rho_max - rho_crit)
+        if type == 'in':
+            return cs.fmin(term1, C * cs.fmin(r, term3))
+        return r * cs.fmin(term1, C * cs.fmin(1, term3))
 
 
 CSTYPES: Dict[str, Type] = {
@@ -9,7 +71,7 @@ CSTYPES: Dict[str, Type] = {
 }
 
 
-class CasadiEngine(EngineBase[Union[cs.SX, cs.MX]]):
+class Engine(EngineBase):
     '''Symbolic engine implemented with the CasADi framework'''
 
     def __init__(self, type: Literal['SX', 'MX'] = 'SX') -> None:
@@ -35,27 +97,30 @@ class CasadiEngine(EngineBase[Union[cs.SX, cs.MX]]):
                 f'got {type} instead.')
         self.CSXX = CSTYPES[type]
 
+    @property
+    def nodes(self) -> Type[NodesEngine]:
+        return NodesEngine
+
+    @property
+    def links(self) -> Type[LinksEngine]:
+        return LinksEngine
+
+    @property
+    def origins(self) -> Type[OriginsEngine]:
+        return OriginsEngine
+
     def var(
         self,
         name: str,
         shape: Tuple[int, int],
         *args,
         **kwargs
-    ) -> Union[cs.SX, cs.MX]:
+    ) -> cs.SX:
         assert len(shape) <= 2, 'CasADi supports 1D and 2D variables only.'
         return self.CSXX.sym(name, *shape)
 
-    def Veq(
-        self,
-        rho: Union[cs.SX, cs.MX],
-        v_free: Union[cs.SX, cs.MX],
-        rho_crit: Union[cs.SX, cs.MX],
-        a: Union[cs.SX, cs.MX]
-    ) -> Union[cs.SX, cs.MX]:
-        return v_free * cs.exp((-1 / a) * cs.power(rho / rho_crit, a))
-
     def __str__(self) -> str:
-        return f'{self.__class__.__name__}'
+        return f'{self.__class__.__name__}(casadi)'
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}(type={self.CSXX.__name__})'
+        return f'{self.__class__.__name__}(casadi, type={self.CSXX.__name__})'
--- src/sym_metanet/engines/core.py
+++ src/sym_metanet/engines/core.py
@@ -1,12 +1,9 @@
-from abc import ABC, abstractmethod, abstractstaticmethod, abstractproperty
-from typing import Dict, Generic, Tuple, Type, TypeVar, Union
+from abc import ABC, abstractmethod
+from typing import Dict, Literal, Tuple, Type, Union
 import sym_metanet
 
 
-T = TypeVar('T')
-
-
-class NodesEngineBase(ABC, Generic[T]):
+class NodesEngineBase(ABC):
     '''
     Abstract class of a symbolic engine for modelling highway nodes via the 
     METANET framework. The methods of this class implement the various 
@@ -19,7 +16,7 @@ class NodesEngineBase(ABC, Generic[T]):
     '''
 
 
-class LinksEngineBase(ABC, Generic[T]):
+class LinksEngineBase(ABC):
     '''
     Abstract class of a symbolic engine for modelling highway link via the 
     METANET framework. The methods of this class implement the various 
@@ -31,15 +28,126 @@ class LinksEngineBase(ABC, Generic[T]):
         control measures", Netherlands TRAIL Research School.
     '''
 
-    @abstractstaticmethod
-    def Veq(rho: T, v_free: T, rho_crit: T, a: T) -> T:
+    @staticmethod
+    @abstractmethod
+    def get_flow(rho, v, lanes):
+        '''Computes the flows of the link's segments, according to 
+        [1, Equation 3.1].
+
+        Parameters
+        ----------
+        rho
+            Densities of the link's segments.
+        v_free
+            Speeds of the link's segments.
+        lanes
+            Number of lanes in the link.
+
+        Returns
+        -------
+        flow
+            The flow in each link's segment.
+        '''
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def step_density(rho, q, q_up, lanes, L, T):
+        '''Computes the densities of the link's segments at the next time 
+        instant, according to [1, Equation 3.2].
+
+        Parameters
+        ----------
+        rho
+            Densities of this link's segments.
+        q
+            Flows of this link's segments.
+        q_up
+            Flows of the upstream link's segments.
+        lanes
+            Number of lanes in the link.
+        L
+            Link's segment length.
+        T
+            Sampling time.
+
+        Returns
+        -------
+        densities_next
+            The density in each link's segment at the next time instant.
+        '''
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def step_speed(v, v_up, rho, rho_down, Veq, lanes, L, tau, eta, kappa, T,
+                   q_ramp=None, delta=None,
+                   lanes_drop=None, phi=None, rho_crit=None):
+        '''Computes the speeds of the link's segments at the next time instant,
+        according to [1, Equation 3.3]. If `q_ramp` and `delta` are provided, 
+        then it also accounts for merging phenomum [1, Equation 3.7]. 
+        Similarly, if `lane_drop` and `phi` are provided, then it also accounts 
+        for lane drops in the incoming link [1, Equation 3.8].
+
+        Parameters
+        ----------
+        v
+            Speeds of this link's segments.
+        v_up
+            Speeds of the upstream link's segments.
+        rho
+            Densities of this link's segments.
+        rho_down
+            Densities of the downstream link's segments.
+        Veq
+            Equilibrium speed of this link.
+        lanes
+            Number of lanes in the link.
+        L
+            Link's segment length.
+        tau
+            Model parameter for the relaxation term.
+        eta
+            Model parameter for the anticipation term.
+        kappa
+            Model parameter for the anticipation term.
+        T
+            Sampling time.
+        q_ramp : optional
+            If provided, it represents the flow of an on-ramp attached to the 
+            first segment of this link, and is used to compute merging 
+            phenomenum of on-ramp traffic with link traffic.
+        delta : optional
+            Model parameter for merging phenomenum. Necessary only if `q_ramp` 
+            is provided as well. 
+        lanes_drop : optional
+            If provided, it represents the difference of lanes of this link 
+            minus the lanes in the next link, and is used to compute the impact
+            of the lane drop on speeds.
+        phi : optional
+            Model parameter for lane drop phenomenum. Necessary only if 
+            `lanes_drop` and `rho_crit` are provided as well.
+        rho_crit : optional 
+            Critical density of the link. Necessary only if `lanes_drop` 
+            and `phi` are provided as well.
+
+        Returns
+        -------
+        speeds_next
+            The speed in each link's segment at the next time instant.
+        '''
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def Veq(rho, v_free, rho_crit, a):
         '''Computes the equilibrium speed of the link, according to 
         [1, Equation 3.4].
 
         Parameters
         ----------
         rho
-            Densities of the link.
+            Densities of the link's segments.
         v_free
             Free-flow speed of the link.
         rho_crit
@@ -55,7 +163,7 @@ class LinksEngineBase(ABC, Generic[T]):
         pass
 
 
-class OriginsEngineBase(ABC, Generic[T]):
+class OriginsEngineBase(ABC):
     '''
     Abstract class of a symbolic engine for modelling highway origins via the 
     METANET framework. The methods of this class implement the various 
@@ -67,8 +175,70 @@ class OriginsEngineBase(ABC, Generic[T]):
         control measures", Netherlands TRAIL Research School.
     '''
 
+    @staticmethod
+    @abstractmethod
+    def step_queue(w, d, q, T):
+        '''Computes the queue of the origin at the next time instant, according
+        to [1, Section 3.2.1].
 
-class EngineBase(ABC, Generic[T]):
+        Parameters
+        ----------
+        w
+            Queue of this origin.
+        d
+            Demand at this origin.
+        q
+            Outflow from this origin.
+        T
+            Sampling time.
+
+        Returns
+        -------
+        queue_next
+            The queue of the origin at the next time instant.
+        '''
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def get_flow(d, w, C, r, rho_max, rho_first, rho_crit, T,
+                 type: Literal['in', 'out'] = 'out'):
+        '''Computes the flows of the origin, according to 
+        [1, Equation 3.5], if `type=='in'`, or [1, Equation 3.6], if 
+        `type=='out'`.
+
+        Parameters
+        ----------
+        d
+            Demand at the origin.
+        w
+            Queue of the origin.
+        C
+            Capacity of the origin.
+        r
+            Metering rate (control action) of the origin.
+        rho_first
+            Density of the first segment of the link the origin is attached to.
+        rho_max
+            Maximum density of the link the origin is attached to.
+        rho_crit
+            Critical density of the link the origin is attached to.
+        T
+            Sampling time.
+        type : 'in' or 'out', optional
+            Whether the metering rate `r` should be inside or outside the min 
+            function, by default 'out'. See Equation 3.5 and  3.6 for more 
+            details.
+
+        Returns
+        -------
+        flow
+            The flow of the origin.
+        '''
+        pass
+
+
+class EngineBase(ABC):
     '''
     Abstract class of a symbolic engine for modelling highways via the 
     METANET framework. The methods of this class implement the various 
@@ -80,20 +250,23 @@ class EngineBase(ABC, Generic[T]):
         control measures", Netherlands TRAIL Research School.
     '''
 
-    @abstractproperty
+    @property
+    @abstractmethod
     def nodes(self) -> Type[NodesEngineBase]:
         pass
 
-    @abstractproperty
+    @property
+    @abstractmethod
     def links(self) -> Type[LinksEngineBase]:
         pass
 
-    @abstractproperty
+    @property
+    @abstractmethod
     def origins(self) -> Type[OriginsEngineBase]:
         pass
 
     @abstractmethod
-    def var(self, name: str, shape: Tuple[int, ...], *args, **kwargs) -> T:
+    def var(self, name: str, shape: Tuple[int, ...], *args, **kwargs):
         '''Creates a variable.
 
         Parameters
