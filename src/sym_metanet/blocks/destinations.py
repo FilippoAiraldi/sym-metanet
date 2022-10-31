06c7d7f34041ef233fca28794d7653630b601e70
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-10-31 12:03:50 +0100

    created congested destination

--- src/sym_metanet/blocks/destinations.py
+++ src/sym_metanet/blocks/destinations.py
@@ -2,5 +2,16 @@ from sym_metanet.util import NamedObject
 
 
 class Destination(NamedObject):
-    '''Congestion-free destination of the highway traffic'''
-    # TODO: improve doc; add congested destination
+    '''
+    Highway congestion-free destination, representing a sink where cars can 
+    leave the highway with no congestion (i.e., slowing down due to downstream 
+    density).
+    '''
+
+
+class CongestedDestination(Destination):
+    '''
+    Destination with a downstream density scenario to emulate congestions, that
+    is, cars cannot exit freely the highway but must slow down and, possibly,
+    create a congestion.
+    '''
