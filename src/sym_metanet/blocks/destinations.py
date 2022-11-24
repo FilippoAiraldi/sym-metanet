ff89d9dfff49a9be4023a682c9c963aff25f938a
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-11-24 11:01:09 +0100

    initializing downstream density in destinations

--- src/sym_metanet/blocks/destinations.py
+++ src/sym_metanet/blocks/destinations.py
@@ -1,6 +1,9 @@
 from typing import Dict, TYPE_CHECKING
 from sym_metanet.blocks.base import ElementBase, sym_var
 from sym_metanet.engines.core import EngineBase, get_current_engine
+if TYPE_CHECKING:
+    from sym_metanet.network import Network
+    from sym_metanet.blocks.links import Link
 
 
 class Destination(ElementBase[sym_var]):
@@ -11,6 +14,11 @@ class Destination(ElementBase[sym_var]):
     '''
 
     def init_vars(self, *args, **kwargs) -> None:
+        '''Initializes no variable in the ideal destination.'''
+        pass
+
+    def step(self, *args, **kwargs) -> None:
+        '''No dynamics to steps in the ideal destination.'''
         pass
 
 
@@ -23,6 +31,7 @@ class CongestedDestination(Destination[sym_var]):
 
     def init_vars(
         self,
+        net: 'Network',
         init_conditions: Dict[str, sym_var] = None,
         engine: EngineBase = None,
         **kwargs
@@ -32,9 +41,8 @@ class CongestedDestination(Destination[sym_var]):
 
         Parameters
         ----------
-        init_d : sym_var, optional
-            The initial density scenario. If `None`, it is automatically
-            initialized.
+        net : Network
+            The network this destination belongs to.
         init_conditions : dict[str, variable], optional
             Provides name-variable tuples to initialize states, actions and
             disturbances with specific values. These values must be compatible
@@ -45,8 +53,18 @@ class CongestedDestination(Destination[sym_var]):
         '''
         if engine is None:
             engine = get_current_engine()
-        self.vars = {
-            'd': engine.var('d')
+        d = (
+            engine.var('d')
             if init_conditions is None or 'd' not in init_conditions else
             init_conditions['d']
-        }
+        )
+        up_links = net.out_links(net.origins[self])
+        assert len(up_links) == 1, \
+            'Internal error. Only one link can enter a destination.'
+        up_link: 'Link' = next(iter(up_links))[-1]
+        rho = engine.destinations.get_congested_downstream_density(
+            rho_last=up_link.vars['rho'][-1],
+            rho_crit=up_link.rho_crit,
+            rho_destination=d
+        )
+        self.vars = {'d': d, 'rho': rho}
