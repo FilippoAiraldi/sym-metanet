00c3a201e5d6a325655869b55fa7448afe189a31
Filippo Airaldi <filippoairaldi@gmail.com>
2022-11-28 12:48:23 +0100

    implemented casadi to_function and tested

--- src/sym_metanet/engines/casadi.py
+++ src/sym_metanet/engines/casadi.py
@@ -1,12 +1,16 @@
-from typing import Dict, Literal, Type, Generic, TypeVar, Union
+from itertools import product
+from typing import Any, Dict, Literal, Type, Generic, TypeVar, Union, \
+    TYPE_CHECKING
 import casadi as cs
 from sym_metanet.engines.core import (
     NodesEngineBase,
     LinksEngineBase,
     OriginsEngineBase,
     DestinationsEngineBase,
-    EngineBase
+    EngineBase,
 )
+if TYPE_CHECKING:
+    from sym_metanet.network import Network
 
 
 csTYPES: Dict[str, Type] = {
@@ -153,8 +157,207 @@ class Engine(EngineBase, Generic[csXX]):
     def vcat(self, *arrays):
         return cs.vertcat(*arrays)
 
+    def to_function(
+        self,
+        net: 'Network',
+        compact: int = 0,
+        more_out: bool = False,
+        force_positive_speed: bool = True,
+        parameters: Dict[str, csXX] = None,
+        **other_parameters
+    ) -> cs.Function:
+        '''Converts the network's dynamics to a CasADi Function.
+
+        Parameters
+        ----------
+        net : Network
+            The network whose dynamics must be translated into a function.
+        compact : int, optional
+            The compactness of input and output arguments. The levels are
+
+            - <= 0: no aggregation of arguments, i.e., the function keeps
+            states, action or disturbances for each element separate.
+
+            - == 1: some aggregation, i.e., same types of variables are clumped
+            together.
+
+            -  > 1: most aggregation, i.e., states, action and disturbances are
+            aggregated in a single vector each.
+
+        more_out : bool, optional
+            Includes flows of links and origins in the output. By default
+            `False`.
+        force_positive_speed : bool, optional
+            If `True`, the links speeds at the next time step are forced to be
+            positive as `v+ = max(0, v+)`. METANET is in fact known to sometime
+            yield negative speeds, which are infeasible.
+        parameters : dict[str, casadi.SX or MX], optional
+            Symbolic network parameters to be included in the function, by
+            default None.
+        **other_parameters
+            Other parameters (numerical or symbolical) required during the
+            computations, e.g., sampling time T is usually required.
+
+        Returns
+        -------
+        cs.Function
+            The CasADi Function representing the network's dynamics.
+
+        Raises
+        ------
+        RuntimeError
+            Raises if variables have not yet been initialized; or if the
+            dynamics have not been stepped yet, so no state at the next time
+            instant is found.
+        '''
+        for el, group in product(net.elements,
+                                 ['_states', '_actions', '_disturbances']):
+            if any(getattr(el, group)) and not getattr(el, f'has{group}'):
+                raise RuntimeError(
+                    f'Found no {group[1:-1]} in {el.name}; perhaps variables '
+                    'have not been initialized via `net.init_vars`?')
+            if any(el._states) and not el.has_next_states:
+                raise RuntimeError(
+                    f'Found no next state in {el.name}; perhaps dynamics have '
+                    'not been stepped via `net.step`?')
+
+        if parameters is None:
+            parameters = {}
+
+        # process inputs
+        x = {el: _filter_vars(vars) for el, vars in net.states.items()}
+        u = {el: _filter_vars(vars) for el, vars in net.actions.items()}
+        d = {el: _filter_vars(vars) for el, vars in net.disturbances.items()}
+
+        # gather inputs
+        names_in, args_in = [], []
+        if compact <= 0:
+            for vars_in in (x, u, d):
+                for el, vars in vars_in.items():
+                    for varname, var in vars.items():
+                        names_in.append(f'{varname}_{el.name}')
+                        args_in.append(var)
+        else:
+            states, actions, disturbances = {}, {}, {}
+            for vars_in, group in [
+                    (x, states), (u, actions), (d, disturbances)]:
+                for el, vars in vars_in.items():
+                    for varname, var in vars.items():
+                        if varname in group:
+                            group[varname].append(var)
+                        else:
+                            group[varname] = [var]
+
+            for group in (states, actions, disturbances):
+                for varname, vars in group.items():
+                    group[varname] = cs.vertcat(*vars)
+
+            if compact == 1:
+                names_in = list(states.keys()) + list(actions.keys()) + \
+                    list(disturbances.keys())
+                args_in = list(states.values()) + list(actions.values()) + \
+                    list(disturbances.values())
+            else:
+                names_in = ['x', 'u', 'd']
+                args_in = [
+                    cs.vertcat(*states.values()),
+                    cs.vertcat(*actions.values()),
+                    cs.vertcat(*disturbances.values())
+                ]
+
+        # process outputs
+        x_next = {el: _filter_vars(vars, independent=False)
+                  for el, vars in net.next_states.items()}
+        if force_positive_speed:
+            for vars in x_next.values():
+                if 'v' in vars:
+                    vars['v'] = cs.fmax(0, vars['v'])
+
+        # gather outputs
+        names_out, args_out = [], []
+        if compact <= 0:
+            for el, vars in x_next.items():
+                for varname, var in vars.items():
+                    names_out.append(f'{varname}_{el.name}+')
+                    args_out.append(var)
+        else:
+            next_states = {}
+            for el, vars in x_next.items():
+                for varname, var in vars.items():
+                    varname += '+'
+                    if varname in next_states:
+                        next_states[varname].append(var)
+                    else:
+                        next_states[varname] = [var]
+
+            for varname, vars in next_states.items():
+                next_states[varname] = cs.vertcat(*vars)
+
+            if compact == 1:
+                names_out = list(next_states.keys())
+                args_out = list(next_states.values())
+            else:
+                names_out = ['x+']
+                args_out = [cs.vertcat(*next_states.values())]
+
+        # add link and origin flows (q_l, q_o) to output
+        if more_out:
+            names_link, flows_link = [], []
+            names_origins, flows_origins = [], []
+            for _, _, link in net.links:
+                names_link.append(f'q_{link.name}')
+                flows_link.append(link.get_flow(engine=self))
+            for origin in net.origins:
+                names_origins.append(f'q_o_{origin.name}')
+                flows_origins.append(
+                    origin.get_flow(net=net, engine=self,
+                                    **parameters, **other_parameters))
+
+            if compact > 0:
+                names_link = ['q']
+                flows_link = [cs.vertcat(*flows_link)]
+                names_origins = ['q_o']
+                flows_origins = [cs.vertcat(*flows_origins)]
+            if compact > 1:
+                names_link = ['q']
+                flows_link = [cs.vertcat(flows_link[0], flows_origins[0])]
+                names_origins, flows_origins = [], []
+            names_out += names_link + names_origins
+            args_out += flows_link + flows_origins
+
+        # add parameters
+        if len(parameters) > 0:
+            if compact <= 0:
+                names_in.extend(parameters.keys())
+                args_in.extend(parameters.values())
+            else:
+                names_in.append('p')
+                args_in.append(cs.vertcat(*parameters.values()))
+
+        return cs.Function('F', args_in, args_out, names_in, names_out)
+
     def __str__(self) -> str:
         return f'{self.__class__.__name__}(casadi)'
 
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}(casadi, type={self._csXX.__name__})'
+
+
+def _filter_vars(
+    vars: Dict[str, Union[csXX, Any]],
+    symbolic: bool = True,
+    independent: bool = True
+) -> Dict[str, csXX]:
+    '''Internal utility to filter out symbols that are either only symbolic
+    and/or independent (and thus can be inputs to  `casadi.Function`).'''
+
+    def is_ok(var: Union[csXX, Any]) -> bool:
+        if not symbolic:
+            return True
+        if not isinstance(var, (cs.SX, cs.MX)):
+            return False
+        if independent:
+            return all(var[i].n_dep() == 0 for i in range(var.shape[0]))
+        return True
+
+    return {name: var for name, var in vars.items() if is_ok(var)}
--- tests/test_engines.py
+++ tests/test_engines.py
@@ -1,29 +1,168 @@
 import unittest
+import sys
+import numpy as np
+import casadi as cs
+
+sys.path.insert(1, "src")
 import sym_metanet as metanet
 from sym_metanet.errors import EngineNotFoundError
 from sym_metanet.util.funcs import first
+from sym_metanet import (
+    Node,
+    Link,
+    MeteredOnRamp,
+    SimpleMeteredOnRamp,
+    Network,
+    CongestedDestination,
+    engines,
+)
+
+
+def get_net():
+    L = 1
+    lanes = 2
+    C = (3500, 2000)
+    tau = 18 / 3600
+    kappa = 40
+    eta = 60
+    rho_max = 180
+    delta = 0.0122
+    T = 10 / 3600
+    a_sym = cs.SX.sym("a")
+    v_free_sym = cs.SX.sym("v_free")
+    rho_crit_sym = cs.SX.sym("rho_crit_sym")
+    N1 = Node(name="N1")
+    N2 = Node(name="N2")
+    N3 = Node(name="N3")
+    L1 = Link(2, lanes, L, rho_max, rho_crit_sym, v_free_sym, a_sym, name="L1")
+    L2 = Link(1, lanes, L, rho_max, rho_crit_sym, v_free_sym, a_sym, name="L2")
+    O1 = MeteredOnRamp(C[0], name="O1")
+    O2 = SimpleMeteredOnRamp(C[1], name="O2")
+    D1 = CongestedDestination(name="D1")
+    net = (
+        Network(name="A1")
+        .add_path(origin=O1, path=(N1, L1, N2, L2, N3), destination=D1)
+        .add_origin(O2, N2)
+    )
+    sym_pars = {"rho_crit": rho_crit_sym, "a": a_sym, "v_free": v_free_sym}
+    others = {"T": T, "tau": tau, "eta": eta, "kappa": kappa, "delta": delta}
+    return net, sym_pars, others
 
 
 class TestEngines(unittest.TestCase):
     def test_get_current_engine__gets_correct_engine(self):
-        self.assertIs(metanet.engines.get_current_engine(), metanet.engine)
+        self.assertIs(engines.get_current_engine(), metanet.engine)
 
     def test_get_available_engines__gets_available_engines_correctly(self):
-        engines = metanet.engines.get_available_engines()
-        self.assertIsInstance(engines, dict)
-        self.assertIsInstance(first(engines.keys()), str)
+        engines_ = engines.get_available_engines()
+        self.assertIsInstance(engines_, dict)
+        self.assertIsInstance(first(engines_.keys()), str)
 
     def test_use__sets_the_engine_correctly(self):
         old_engine = metanet.engine
-        new_engine = metanet.engines.use('casadi')
+        new_engine = engines.use("casadi")
         self.assertIsNot(metanet.engine, old_engine)
         self.assertIs(metanet.engine, new_engine)
 
     def test_use__raises__when_engine_not_found(self):
         invalid_engine = object()
         with self.assertRaises(EngineNotFoundError):
-            metanet.engines.use(invalid_engine)
+            engines.use(invalid_engine)
 
 
-if __name__ == '__main__':
+class TestCasadiEngine(unittest.TestCase):
+    def test_to_function__fails__with_uninit_vars(self):
+        net, sym_pars, other_pars = get_net()
+        self.assertTrue(net.is_valid(raises=False)[0])
+        engine = engines.use("casadi", sym_type="SX")
+
+        with self.assertRaises(RuntimeError):
+            engine.to_function(
+                net=net,
+                parameters=sym_pars,
+                T=other_pars["T"],
+                compact=1,
+                more_out=True,
+            )
+
+    def test_to_function__fails__with_unstepped_dynamics(self):
+        net, sym_pars, other_pars = get_net()
+        self.assertTrue(net.is_valid(raises=False)[0])
+        engine = engines.use("casadi", sym_type="SX")
+        for el in net.elements:
+            el.init_vars(engine=engine)
+        with self.assertRaises(RuntimeError):
+            engine.to_function(
+                net=net,
+                parameters=sym_pars,
+                T=other_pars["T"],
+                compact=1,
+                more_out=True,
+            )
+
+    def test_to_function__numerically_works(self):
+        L = 1
+        lanes = 2
+        C = (3500, 2000)
+        tau = 18 / 3600
+        kappa = 40
+        eta = 60
+        rho_max = 180
+        delta = 0.0122
+        T = 10 / 3600
+        a = cs.SX.sym("a")
+        v_free = cs.SX.sym("v_free")
+        rho_crit = cs.SX.sym("rho_crit_sym")
+        N1 = Node(name="N1")
+        N2 = Node(name="N2")
+        N3 = Node(name="N3")
+        L1 = Link(2, lanes, L, rho_max, rho_crit, v_free, a, name="L1")
+        L2 = Link(1, lanes, L, rho_max, rho_crit, v_free, a, name="L2")
+        O1 = MeteredOnRamp(C[0], name="O1")
+        O2 = SimpleMeteredOnRamp(C[1], name="O2")
+        D1 = CongestedDestination(name="D1")
+        net = (
+            Network()
+            .add_path(origin=O1, path=(N1, L1, N2, L2, N3), destination=D1)
+            .add_origin(O2, N2)
+        )
+        net.is_valid(raises=True)
+        net.step(T=T, tau=tau, eta=eta, kappa=kappa, delta=delta)
+        args = {
+            "net": net,
+            "more_out": True,
+            "T": T,
+            "parameters": {"rho_crit": rho_crit, "a": a, "v_free": v_free},
+        }
+        Factual = metanet.engine.to_function(**args, compact=1)
+        Fexpected = cs.Function.load(r"tests\data\F.casadi")
+
+        for _ in range(1_000):
+            p = [
+                np.maximum(4, np.random.randn() * 5 + 20),  # rho_crit
+                np.maximum(1, np.random.randn() * 0.5 + 2),  # a
+                np.maximum(10, np.random.randn() * 10 + 80),  # v_free
+            ]
+            rho = np.maximum(4, np.random.randn(3) * 5 + 20)
+            v = np.maximum(10, np.random.randn(3) * 10 + 80)
+            w = np.maximum(0, np.random.randn(2) * 3 + 10)
+            d = np.maximum(0, np.random.randn(3) * 100 + 1000)
+            q = np.maximum(0, np.random.randn() * 100 + 1000)
+            q = np.minimum(q, d[1] + w[1] / T)
+            q = np.minimum(q, C[1] * (rho_max - rho[2]) / (rho_max - p[0]))
+
+            rho_next1, v_next1, w_next1, q1, q_o1 = Factual(rho, v, w, 1, q, d, p)
+            q_o2, w_next2, q2, rho_next2, v_next2 = Fexpected(w, rho, v, q, d, *p)
+
+            for name, x, y in [
+                ("rho", rho_next1, rho_next2),
+                ("v", v_next1, v_next2),
+                ("w", w_next1, w_next2),
+                ("q", q1, q2),
+                ("q_o", q_o1, q_o2),
+            ]:
+                np.testing.assert_allclose(x, y, atol=1e-12, rtol=1e-12, err_msg=name)
+
+
+if __name__ == "__main__":
     unittest.main()
