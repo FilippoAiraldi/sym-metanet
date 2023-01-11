107856590638396bd26fc960c8ca9a348df3188c
Filippo Airaldi <filippoairaldi@gmail.com>
2023-01-11 16:19:06 +0100

    adjusted tests

--- tests/test_engines.py
+++ tests/test_engines.py
@@ -2,6 +2,7 @@ import unittest
 
 import casadi as cs
 import numpy as np
+from parameterized import parameterized
 
 import sym_metanet as metanet
 from sym_metanet import (
@@ -81,12 +82,20 @@ def get_hardcoded_dynamics(
     q_O1 = r * cs.fmin(
         d[0] + w[0] / T, C[0] * cs.fmin(1, (rho_max - rho[0]) / (rho_max - rho_crit))
     )
-    q_o = cs.vertcat(q_O1, q_O2)
+    q_O2_limited = cs.mmin(
+        cs.vertcat(  # must limit the desired q_O2 to be feasible
+            q_O2,
+            w[1] / T + d[1],
+            C[1],
+            C[1] * (rho_max - rho[1]) / (rho_max - rho_crit),
+        ),
+    )
+    q_o = cs.vertcat(q_O1, q_O2_limited)
     w_next = w + T * (d[:2] - q_o)
 
     q = lanes * rho * v
     q_up = cs.vertcat(q_O1, q[0], q[1])
-    q_up[link_with_ramp] += q_O2
+    q_up[link_with_ramp] += q_O2_limited
     v_up = cs.vertcat(v[0], v[0], v[1])
     rho_down = cs.vertcat(rho[1], rho[2], cs.fmax(cs.fmin(rho[2], rho_crit), d[2]))
     Veq = v_free * cs.exp((-1 / a) * ((rho / rho_crit)) ** a)
@@ -97,12 +106,16 @@ def get_hardcoded_dynamics(
         + T / L * v * (v_up - v)
         - eta * T / tau / L * (rho_down - rho) / (rho + kappa)
     )
-    v_next[link_with_ramp] = v_next[link_with_ramp] - delta * T / L / lanes * q_O2 * v[
-        link_with_ramp
-    ] / (rho[link_with_ramp] + kappa)
-    v_next = cs.fmax(0, v_next)
+    merging_num = delta * T * q_O2_limited * v[link_with_ramp]
+    merging_den = L * lanes * (rho[link_with_ramp] + kappa)
+    v_next[link_with_ramp] -= merging_num / merging_den
 
     pars = cs.vertcat(rho_crit, a, v_free)
+    # rho_next = cs.fmax(0, rho_next)
+    v_next = cs.fmax(0, v_next)
+    # w_next = cs.fmax(0, w_next)
+    # q = cs.fmax(0, q)
+    # q_o = cs.fmax(0, q_o)
     return cs.Function(
         "F", [rho, v, w, r, q_O2, d, pars], [rho_next, v_next, w_next, q, q_o]
     )
@@ -159,48 +172,63 @@ class TestCasadiEngine(unittest.TestCase):
                 more_out=True,
             )
 
-    def test_to_function__numerically_works(self):
-        for i in (1, 2):
-            net, sym_pars, other_pars = get_net(link_with_ramp=i)
-            net.is_valid(raises=True)
-            net.step(**other_pars)
-            args = {
-                "net": net,
-                "more_out": True,
-                **other_pars,
-                "parameters": sym_pars,
-            }
-            F1: cs.Function = metanet.engine.to_function(**args, compact=1)
-            F2 = get_hardcoded_dynamics(**other_pars, link_with_ramp=i)
+    @parameterized.expand([(1,), (2,)])
+    def test_to_function__numerically_works(self, link_with_ramp: int):
+        net, sym_pars, other_pars = get_net(link_with_ramp)
+        net.is_valid(raises=True)
+        net.step(**other_pars)
+        Fact: cs.Function = metanet.engine.to_function(
+            net=net,
+            **other_pars,
+            parameters=sym_pars,
+            more_out=True,
+            force_positive_speed=True,
+            compact=1,
+        )
+        Fexp = get_hardcoded_dynamics(**other_pars, link_with_ramp=link_with_ramp)
 
-            names = F1.name_out()
-            for _ in range(1_000):
-                rho = np.random.randn(3) * 5 + 30
-                v = np.random.randn(3) * 10 + 100
-                w = np.random.randn(2) * 30 + 100
-                r = np.random.rand()
-                q = np.random.randn() * 100 + 800
-                d = np.hstack(
-                    (np.random.randn(2) * 200 + 1000, np.random.randn() * 20 + 50)
+        np_random = np.random.Generator(np.random.PCG64(69420))
+        names = Fact.name_out()
+        for i in range(1_000):
+            if i == 0:
+                rho = np.asarray([10, 10, 10])
+                v = np.asarray([100, 100, 100])
+                w = np.asarray([0, 0])
+                r = 1.0
+                q = 575.0
+                d = np.asarray([1027.85313984, 391.81732715, 21.76034038])
+                a = 1.867
+                v_free = 102.0
+                rho_crit = 33.5
+            else:
+                rho = np_random.normal(30, 10, 3)
+                v = np_random.normal(100, 30, 3)
+                w = np_random.normal(100, 50, 2)
+                r = np_random.random()
+                q = np_random.normal(800, 200)
+                d = np_random.normal((1e3, 1e3, 50), (200, 200, 20))
+                rho_crit = np_random.uniform(10, 30)
+                a = np_random.uniform(1, 2)
+                v_free = np_random.uniform(40, 80)
+            args = [
+                np.maximum(0, arg)
+                for arg in [rho, v, w, r, q, d, [rho_crit, a, v_free]]
+            ]
+            out_exp = Fexp(*args)
+            out_act = Fact(*args)
+
+            for name, exp_, act_ in zip(names, out_exp, out_act):
+                exp: np.ndarray = exp_.full().flatten()
+                act: np.ndarray = act_.full().flatten()
+                self.assertTrue((exp >= 0).all(), f"[exp] {name} negative (iter {i})")
+                self.assertTrue((act >= 0).all(), f"[act] {name} negative (iter {i})")
+                np.testing.assert_allclose(
+                    act,
+                    exp,
+                    atol=1e-6,
+                    rtol=1e-6,
+                    err_msg=f"{name} dissimilar (iter {i})",
                 )
-                rho_crit = np.random.rand() * 10 + 30
-                a = np.random.rand() + 1
-                v_free = np.random.rand() * 40 + 80
-                args = [
-                    np.maximum(0, arg)
-                    for arg in [rho, v, w, r, q, d, [rho_crit, a, v_free]]
-                ]
-                out1 = F1(*args)
-                out2 = F2(*args)
-
-                for name, x, y in zip(names, out1, out2):
-                    np.testing.assert_allclose(
-                        x.full().flatten(),
-                        y.full().flatten(),
-                        atol=1e-6,
-                        rtol=1e-6,
-                        err_msg=name,
-                    )
 
 
 class TestCasadiVsNumpyEngine(unittest.TestCase):
