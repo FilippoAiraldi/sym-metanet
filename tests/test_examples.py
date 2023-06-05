0af00852ad607965583af51f95e9e5f48f549bf7
Filippo Airaldi <filippoairaldi@gmail.com>
2023-06-05 16:06:00 +0200

    implemented test for coordinated control

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -8,7 +8,15 @@ from csnlp import Nlp
 from csnlp.wrappers import Mpc
 from parameterized import parameterized_class
 
-from sym_metanet import Destination, Link, MeteredOnRamp, Network, Node, engines
+from sym_metanet import (
+    Destination,
+    Link,
+    LinkWithVsl,
+    MeteredOnRamp,
+    Network,
+    Node,
+    engines,
+)
 
 with open("tests/data_test_examples.pkl", "rb") as f:
     RESULTS = pickle.load(f)
@@ -23,7 +31,7 @@ def create_demands(time: np.ndarray) -> np.ndarray:
     )
 
 
-def get_net() -> Tuple[Network, Dict[str, Any]]:
+def get_net(vsl: bool = False) -> Tuple[Network, Dict[str, Any]]:
     T = 10 / 3600
     Tfin = 2.5
     time = np.arange(0, Tfin, T)
@@ -44,8 +52,14 @@ def get_net() -> Tuple[Network, Dict[str, Any]]:
     O1 = MeteredOnRamp(C[0], name="O1")
     O2 = MeteredOnRamp(C[1], name="O2")
     D1 = Destination(name="D1")
-    L1 = Link(4, lanes, L, rho_max, rho_crit, v_free, a, name="L1")
-    L2 = Link(2, lanes, L, rho_max, rho_crit, v_free, a, name="L2")
+    args = (lanes, L, rho_max, rho_crit, v_free, a)
+    if vsl:
+        L1 = LinkWithVsl[cs.SX](
+            4, *args, segments_with_vsl={2, 3}, alpha=0.1, name="L1"
+        )
+    else:
+        L1 = Link(4, *args, a, name="L1")
+    L2 = Link(2, *args, name="L2")
     net = (
         Network(name="A1")
         .add_path(origin=O1, path=(N1, L1, N2, L2, N3), destination=D1)
@@ -124,7 +138,7 @@ class TestExamples(unittest.TestCase):
             kappa=pars["kappa"],
             delta=pars["delta"],
             engine=engine,
-            init_conditions={net.origins_by_name["O1"]: {"r": 1}}
+            init_conditions={net.origins_by_name["O1"]: {"r": 1}},
         )
         F = engine.to_function(
             net=net,
@@ -204,6 +218,109 @@ class TestExamples(unittest.TestCase):
         np.testing.assert_allclose(Q, Q_, rtol=1e-3, atol=1e-3)
         np.testing.assert_allclose(Q_o, Q_o_, rtol=1e-3, atol=1e-3)
         np.testing.assert_allclose(R, R_, rtol=1e-3, atol=1e-3)
+        np.testing.assert_allclose(tts, tts_, rtol=1e-3, atol=1e-3)
+
+    def test_coordinated_control_example(self):
+        engine = engines.use("casadi", sym_type=self.sym_type)
+        net, pars = get_net(vsl=True)
+        net.step(
+            T=pars["T"],
+            tau=pars["tau"],
+            eta=pars["eta"],
+            kappa=pars["kappa"],
+            delta=pars["delta"],
+            engine=engine,
+            init_conditions={net.origins_by_name["O1"]: {"r": 1}},
+        )
+        F = engine.to_function(
+            net=net,
+            more_out=True,
+            compact=2,
+            T=pars["T"],
+        )
+        demands = create_demands(pars["time"]).T
+
+        Np, Nc, M = 7, 5, 6
+        mpc = Mpc[cs.SX](
+            nlp=Nlp[cs.SX](sym_type=self.sym_type),
+            prediction_horizon=Np * M,
+            control_horizon=Nc * M,
+            input_spacing=M,
+        )
+        n_seg, n_orig = sum(link.N for _, _, link in net.links), len(net.origins)
+        rho, _ = mpc.state("rho", n_seg, lb=0)
+        v, _ = mpc.state("v", n_seg, lb=0)
+        w, _ = mpc.state(
+            "w", n_orig, lb=0, ub=[[np.inf], [100]]
+        )  # O2 queue is constrained
+        v_ctrl, _ = mpc.action("v_ctrl", 2, lb=20, ub=pars["v_free"])
+        r, _ = mpc.action("r", lb=0, ub=1)
+        mpc.disturbance("d", n_orig)
+        mpc.set_dynamics(F)
+        v_ctrl_last = mpc.parameter("v_ctrl_last", (v_ctrl.size1(), 1))
+        r_last = mpc.parameter("r_last", (r.size1(), 1))
+        mpc.minimize(
+            pars["T"] * cs.sum2(cs.sum1(rho * pars["L"] * pars["lanes"]) + cs.sum1(w))
+            + 0.4
+            * cs.sumsqr(cs.diff(cs.horzcat(v_ctrl_last, v_ctrl), 1, 1) / pars["v_free"])
+            + 0.4 * cs.sumsqr(cs.diff(cs.horzcat(r_last, r)))
+        )
+        opts = {
+            "expand": True,
+            "print_time": False,
+            "ipopt": {"max_iter": 500, "sb": "yes", "print_level": 0},
+        }
+        mpc.init_solver(solver="ipopt", opts=opts)
+
+        rho = cs.DM([22, 22, 22.5, 24, 30, 32])
+        v = cs.DM([80, 80, 78, 72.5, 66, 62])
+        w = cs.DM([0, 0])
+        v_ctrl_last = v[: net.links_by_name["L1"].N][net.links_by_name["L1"].vsl]
+        r_last = cs.DM.ones(r.size1(), 1)
+        sol_prev = None
+        RHO, V, W, Q, Q_o, V_CTRL, R = [], [], [], [], [], [], []
+        for k in range(100):
+            d_hat = demands[k : k + Np * M, :]
+            if k % M == 0:
+                sol = mpc.solve(
+                    pars={
+                        "rho_0": rho,
+                        "v_0": v,
+                        "w_0": w,
+                        "d": d_hat.T,
+                        "v_ctrl_last": v_ctrl_last,
+                        "r_last": r_last,
+                    },
+                    vals0=sol_prev,
+                )
+                sol_prev = sol.vals
+                v_ctrl_last = sol.vals["v_ctrl"][:, 0]
+                r_last = sol.vals["r"][0]
+            x_next, q_all = F(
+                cs.vertcat(rho, v, w), cs.vertcat(v_ctrl_last, r_last), demands[k, :]
+            )
+            rho, v, w = cs.vertsplit(x_next, (0, n_seg, 2 * n_seg, 2 * n_seg + n_orig))
+            q, q_o = cs.vertsplit(q_all, (0, n_seg, n_seg + n_orig))
+            RHO.append(rho)
+            V.append(v)
+            W.append(w)
+            Q.append(q)
+            Q_o.append(q_o)
+            V_CTRL.append(v_ctrl_last)
+            R.append(r_last)
+        RHO, V, W, Q, Q_o, V_CTRL, R = (np.squeeze(o) for o in (RHO, V, W, Q, Q_o, V_CTRL, R))  # type: ignore
+        tts = pars["T"] * sum(
+            (rho * pars["L"] * pars["lanes"]).sum() + w.sum() for rho, w in zip(RHO, W)
+        )
+
+        RHO_, V_, W_, Q_, Q_o_, V_CTRL_, R_, tts_ = RESULTS["test_coord"]
+        np.testing.assert_allclose(RHO, RHO_, rtol=1e-3, atol=1e-3)
+        np.testing.assert_allclose(V, V_, rtol=1e-3, atol=1e-3)
+        np.testing.assert_allclose(W, W_, rtol=1e-3, atol=1e-3)
+        np.testing.assert_allclose(Q, Q_, rtol=1e-3, atol=1e-3)
+        np.testing.assert_allclose(Q_o, Q_o_, rtol=1e-3, atol=1e-3)
+        np.testing.assert_allclose(R, R_, rtol=1e-3, atol=1e-3)
+        np.testing.assert_allclose(V_CTRL, V_CTRL_, rtol=1e-3, atol=1e-3)
         np.testing.assert_allclose(tts, tts_, rtol=1e-3, atol=1e-3)
 
 
